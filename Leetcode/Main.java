b12e0ed795289c1d2b975311e38296391b7d2948
Gyandev J <reddynani683@gmail.com>
2025-09-30 12:56:35 -0400

    added files

--- Leetcode/Main.java
+++ Leetcode/Main.java
@@ -8,7 +8,7 @@ public class Main {
 			}
 			System.out.println();
 		}
-		for (int i = n; i >= 1; i--) {
+		for (int i = n-1; i >= 1; i--) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
